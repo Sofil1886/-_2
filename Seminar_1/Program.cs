c1d2f0e2224bb22fbcf23a03ce935cf09cf789d7
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-27 23:09:23 +0300

    Added Seminar 1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int result = number*number;
+Console.Write(result);
