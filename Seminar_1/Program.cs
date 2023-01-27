78f122dec8039fca8c23f4f3aaa8fa0c5a48016a
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-27 23:23:12 +0300

    solved 2nd task

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,4 +1,22 @@
-﻿System.Console.WriteLine("Введите число:");
-int number = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
-int result = number*number;
-Console.Write(result);
+﻿// System.Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+// int result = number*number;
+// Console.Write(result);
+
+
+
+// Задача 1
+
+System.Console.WriteLine("Введите числа:");
+
+int numberA = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+int numberB = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+
+if ((numberA*numberA == numberB) || (numberB*numberB == numberA))
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
