29324d3bba24c99c16b040194f5133a063f10a1c
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-01-27 23:30:33 +0300

    solved 3rd  task

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -7,12 +7,12 @@
 
 // Задача 1
 
-System.Console.WriteLine("Введите числа:");
+System.Console.WriteLine("Введите два числа:");
 
 int numberA = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
 int numberB = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
 
-if ((numberA*numberA == numberB) || (numberB*numberB == numberA))
+if (numberB*numberB == numberA)
 {
     System.Console.WriteLine("Да");
 }
@@ -20,3 +20,41 @@ else
 {
     System.Console.WriteLine("Нет");
 }
+
+// Задача 3
+
+System.Console.WriteLine("Введите число:");
+int day = Convert.ToInt32(Console.ReadLine()); // int.Parse() || Convert.ToInt32
+
+if (day == 1)
+{
+    System.Console.WriteLine("Понедельник");
+}
+if (day == 2)
+{
+    System.Console.WriteLine("Вторник");
+}
+if (day == 3)
+{
+    System.Console.WriteLine("Среда");
+}
+
+if (day == 4)
+{
+    System.Console.WriteLine("Четверг");
+}
+
+if (day == 5)
+{
+    System.Console.WriteLine("Пятница");
+}
+
+if (day == 6)
+{
+    System.Console.WriteLine("Суббота");
+}
+if (day == 7)
+{
+    System.Console.WriteLine("Воскресенье");
+}
+
