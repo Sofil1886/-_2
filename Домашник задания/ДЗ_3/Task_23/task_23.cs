1729376085638b29819a3eb9c294ac519965a666
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-02-03 14:41:27 +0300

    solved homework

---
+++ Домашник задания/ДЗ_3/Task_23/task_23.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int N = InputNumber("Введите число: ");
+
+void find_res(int number)
+{
+
+    if (number < 1)
+    {
+        System.Console.WriteLine("Введите число, которое больше, либо равно 1");
+    }
+    else
+    {
+        for (int count = 1; count <= N; count += 1)
+        {
+            System.Console.Write(Math.Pow(count, 3) + ", ");
+        }
+    }
+
+}
+
+
+find_res(N);
