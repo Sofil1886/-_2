6717e94c4eb4cf1820e488053e36b5f74cfe4c1b
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-02-08 17:59:30 +0300

    solved homework

---
+++ Домашник задания/ДЗ_4/29/29.cs
@@ -0,0 +1,30 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = GetNumber("Write size of array:");
+int B = GetNumber("Write first number:");
+int C = GetNumber("Write last number:");
+
+int [] GetArray(int size, int leftNumber, int rightNumber)
+{
+    int[] elements = new int[size];
+    for (int i= 0; i < elements.Length; i++)
+    {
+        elements[i] = new Random().Next(leftNumber,rightNumber);
+    }
+
+    return elements;
+}
+
+int[] arr = GetArray(A, B, C);
+
+System.Console.WriteLine($"[{string.Join(", ",arr)}]"); 
