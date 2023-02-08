4b9a7ebeb899d7be33da247665fc4512d5037023
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-02-08 18:00:11 +0300

    solved tasks

---
+++ Домашник задания/ДЗ_4/25/Task_25.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int A = GetNumber("Write number:");
+int B = GetNumber("Write number:");
+
+void product(int X, int Y)
+{
+    int baseNumber = X;
+    for (int i = 1; i < Y; i++)
+    {
+        X = X * baseNumber;
+    }
+    System.Console.WriteLine(X);
+}
+
+product(A, B);
---
+++ Домашник задания/ДЗ_4/27/Task_27.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+string GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Console.ReadLine();
+}
+string num = GetNumber("Write number: ");
+
+void SumResult(string number)
+{   
+    int result = 0;
+    for(int i =0; i < number.Length; i ++)
+    {
+        int convertednumber = Convert.ToInt32(number[i].ToString());
+        result = result + convertednumber;
+    }
+    System.Console.WriteLine(result);
+}
+
+SumResult(num);
+
