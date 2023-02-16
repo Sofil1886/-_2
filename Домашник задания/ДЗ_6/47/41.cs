8684e162dc2b981331a3b5b2afd50da9bcbd883e
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-02-16 22:37:20 +0300

    solved homework

---
+++ Домашник задания/ДЗ_6/47/41.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите числа:");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+
+void Findcount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    System.Console.WriteLine($"Пользователь ввел {count} чисел больше нуля");
+}
+
+
+Findcount(array);
---
+++ Домашник задания/ДЗ_6/50/43.cs
@@ -0,0 +1,20 @@
+﻿double GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double a = GetNumber("Введите b1:");
+double b = GetNumber("Введите k1:");
+double c = GetNumber("Введите b2:");
+double d = GetNumber("Введите k2:");
+
+void FindCoordinates(double b1, double k1, double b2, double k2)
+{
+    double X = (b2 - b1) / (k1 - k2);
+    double Y = k1 * X + b1;
+    
+    System.Console.WriteLine($"({X},{Y})");
+}
+
+FindCoordinates(a, b, c, d);
