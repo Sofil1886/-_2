d8d28438737cfcd4bf2a8bf1611dc6d115852a69
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-03-01 12:25:37 +0300

    solved 3 tasks

---
+++ Домашник задания/ДЗ_8/56/56.cs
@@ -0,0 +1,84 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void WriterowWithlessSum(int[,] matrix)
+{
+    int[] numbers = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+
+        numbers[i] = sum;
+    }
+
+    int max = numbers[0];
+    for (int element = 0; element < numbers.Length; element++)
+    {
+        if (numbers[element] > max)
+        {
+            max = numbers[element];
+        }
+    }
+    int res = Array.IndexOf(numbers, max);
+    System.Console.WriteLine($"{res+1} строка");
+}
+
+
+int rows = ReadInt("Write number of rows in matrix: ");
+int cols = ReadInt("Write number of columns in matrix: ");
+
+var myMatrix = GenerateMatrix(rows, cols);
+
+PrintMatrix(myMatrix);
+System.Console.WriteLine("-------");
+WriterowWithlessSum(myMatrix);
---
+++ Домашник задания/ДЗ_8/58/58.cs
@@ -0,0 +1,93 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+int[,] GenerateMatrix(int row, int col)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] ProdMatrix(int[,] firstmatrix, int[,] secondmatrix)
+{
+
+    if (firstmatrix.GetLength(1) != secondmatrix.GetLength(0))
+    {
+        return null;
+    }
+    else
+    {
+        int[,] newMatrix = new int[firstmatrix.GetLength(0), secondmatrix.GetLength(1)];
+        for (int i = 0; i < firstmatrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < secondmatrix.GetLength(1); j++)
+            {
+                for (int k = 0; k < firstmatrix.GetLength(1); k++)
+                {
+                    newMatrix[i, j] += firstmatrix[i, k] * secondmatrix[k, j];
+                }
+            }
+        }
+        return newMatrix;
+    }
+
+
+}
+
+
+int rows = ReadInt("Write number of rows in matrix: ");
+int cols = ReadInt("Write number of columns in matrix: ");
+
+int rows2 = ReadInt("Write number of rows in matrix: ");
+int cols2 = ReadInt("Write number of columns in matrix: ");
+
+var myMatrix = GenerateMatrix(rows, cols);
+var myMatrix1 = GenerateMatrix(rows2, cols2);
+PrintMatrix(myMatrix);
+System.Console.WriteLine("------");
+PrintMatrix(myMatrix1);
+System.Console.WriteLine("------");
+var result = ProdMatrix(myMatrix, myMatrix1);
+if (result == null)
+{
+    System.Console.WriteLine("Матрицы нельзя перемножить");
+}
+else
+{
+    PrintMatrix(result);
+}
+
+
