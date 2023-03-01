f2774e00ddae0bb919aa450d80fc55f00a6610d4
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-03-01 15:52:58 +0300

    solved last homework

---
+++ Домашник задания/ДЗ_9/66/66.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+void FindSum(int m, int n, int sum = 0)
+{
+    if (m <= n)
+    {   
+        sum += m; 
+        FindSum(m+1, n, sum);      
+    }
+    else
+    {
+        System.Console.WriteLine(sum);
+    }
+    
+}
+
+
+int M = GetNumber("Write number M: ");
+int N = GetNumber("Write number N: ");
+FindSum(M, N);
---
+++ Домашник задания/ДЗ_9/68/68.cs
@@ -0,0 +1,33 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+int M = GetNumber("Введите неотрицатльное число M: ");
+int N = GetNumber("Введите неотрицатльное число N: ");
+
+if ((M < 0) || (N < 0))
+{
+    System.Console.WriteLine("Вы ввели отрицательное число, введите неотрицательное!");
+}
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+
+System.Console.WriteLine(Akkerman(M, N));
